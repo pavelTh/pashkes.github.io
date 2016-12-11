987cec1564b04f917b4330b83957ea07b32787c6
pashkes <pashkes@gmail.com>
2016-12-11 16:12:20 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var acc = document.getElementsByClassName("progects__desc");
+var acc = document.getElementsByClassName("progects__title");
 var i;
 
 for (i = 0; i < acc.length; i++) {
@@ -6,4 +6,26 @@ for (i = 0; i < acc.length; i++) {
         this.classList.toggle("active");
         this.nextElementSibling.classList.toggle("show");
     }
+}
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
